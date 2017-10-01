a38a4529e387218b4d3570ce2406335d2daf67cf
Isashi Mitsui <isashimitsui@gmail.com>
2017-10-01 19:05:35 +0000

    working form and confirmation page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,24 @@
 class BookingsController < ApplicationController
     def new
+        @booking = Booking.new
+        @booking.passengers.build
     end
+    
+    def create
+        @booking = Booking.create(booking_params)
+        if @booking.save 
+            redirect_to @booking
+        else 
+            render :new
+        end
+    end
+    
+    def show
+        @booking = Booking.find(params[:id])
+    end
+    
+    private
+        def booking_params
+            params.required(:booking).permit(:flight_id, :passengers_attributes => [:name, :email])
+        end
 end
